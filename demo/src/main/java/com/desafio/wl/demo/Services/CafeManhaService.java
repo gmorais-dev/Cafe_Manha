c510620d3d9bfc0f4942ecb8700e222e6eaf398c
gmorais-dev <guilherme_morais14@hotmail.com>
2023-11-11 17:09:56 -0300

    coment

--- demo/src/main/java/com/desafio/wl/demo/Services/CafeManhaService.java
+++ demo/src/main/java/com/desafio/wl/demo/Services/CafeManhaService.java
@@ -1,20 +1,74 @@
 package com.desafio.wl.demo.Services;
 
+import com.desafio.wl.demo.Model.CafeManha;
 import com.desafio.wl.demo.Repository.CafeManhaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 public class CafeManhaService {
-    private  CafeManhaRepository CafeManhaRepository;
 
-    public CafeManhaService(CafeManhaRepository CafeManhaRepository) {
-        this.CafeManhaRepository = CafeManhaRepository;
+    private final CafeManhaRepository cafeManhaRepository;
+
+    public CafeManhaService(CafeManhaRepository cafeManhaRepository) {
+        this.cafeManhaRepository = cafeManhaRepository;
     }
 
+    public ResponseEntity<String> adicionarCafeManha(CafeManha cafeManha) {
+        Optional<CafeManha> cafeExistente = Optional.ofNullable(cafeManhaRepository.findByCpfAndData(cafeManha.getCpf(), cafeManha.getData()));
+
+        if (cafeExistente.isPresent()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Já existe um registro para este CPF nesta data.");
+        }
+
+        try {
+            cafeManhaRepository.save(cafeManha);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Registro de café da manhã adicionado com sucesso.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao adicionar registro de café da manhã.");
+        }
+    }
+
+    public ResponseEntity<String> atualizarCafeManha(CafeManha cafeManha) {
+        Optional<CafeManha> cafeExistente = cafeManhaRepository.findById(cafeManha.getId());
+
+        if (cafeExistente.isPresent()) {
+            try {
+
+                CafeManha cafeAtualizado = cafeExistente.get();
+                cafeAtualizado.setNomeColaborador(cafeManha.getNomeColaborador());
+                cafeAtualizado.setCpf(cafeManha.getCpf());
+                cafeAtualizado.setOpcaoCafe(cafeManha.getOpcaoCafe());
+                cafeAtualizado.setData(cafeManha.getData());
 
 
+                cafeManhaRepository.save(cafeAtualizado);
 
+                return ResponseEntity.status(HttpStatus.OK).body("Registro de café da manhã atualizado com sucesso.");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar registro de café da manhã.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registro de café da manhã não encontrado.");
+        }
+    }
 
+    public ResponseEntity<String> excluirCafeManha(String cpf, LocalDate data) {
+        Optional<CafeManha> cafeExistente = Optional.ofNullable(cafeManhaRepository.findByCpfAndData(cpf, data));
 
-
+        if (cafeExistente.isPresent()) {
+            try {
+                cafeManhaRepository.delete(cafeExistente.get());
+                return ResponseEntity.status(HttpStatus.OK).body("Registro de café da manhã excluído com sucesso.");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao excluir registro de café da manhã.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Registro de café da manhã não encontrado para exclusão.");
+        }
+    }
 }
