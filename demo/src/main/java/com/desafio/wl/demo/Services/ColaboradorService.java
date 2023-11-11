6f6ba106c2dc37b105bbf80543873fd4640b5067
gmorais-dev <guilherme_morais14@hotmail.com>
2023-11-11 18:44:11 -0300

    logica do service para implementação do colaborador e cafe da manha

--- demo/src/main/java/com/desafio/wl/demo/Services/ColaboradorService.java
+++ demo/src/main/java/com/desafio/wl/demo/Services/ColaboradorService.java
@@ -3,7 +3,6 @@ package com.desafio.wl.demo.Services;
 import com.desafio.wl.demo.Model.CafeManha;
 import com.desafio.wl.demo.Model.Colaborador;
 import com.desafio.wl.demo.Repository.ColaboradorRepository;
-import com.desafio.wl.demo.Services.CafeManhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +30,33 @@ public class ColaboradorService {
                 cafeManha.setNomeColaborador(String.valueOf(colaboradorExistente.get()));
                 cafeManhaService.adicionarCafeManha(cafeManha);
                 return ResponseEntity.status(HttpStatus.CREATED).body("Café da Manhã associado ao Colaborador com sucesso");
+            } catch (Exception e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Erro ao associar Café da Manhã ao Colaborador.");
+            }
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Colaborador não encontrado na nossa base de dados");
+        }
+    }
+
+    private boolean validarCPF(String cpf) {
+        return cpf != null && cpf.matches("\\d{11}");
+    }
+
+    public ResponseEntity<String> validarEAdicionarCafeManha(Long colaboradorId, CafeManha cafeManha) {
+        Optional<Colaborador> colaboradorExistente = colaboradorRepository.findById(colaboradorId);
+
+        if (colaboradorExistente.isPresent()) {
+            try {
+                String cpf = colaboradorExistente.get().getCpf();
+                if (!validarCPF(cpf)) {
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro ao associar Café da Manhã ao Colaborador. CPF inválido.");
+                }
+
+                cafeManha.setNomeColaborador(colaboradorExistente.get().getNome());
+                cafeManhaService.adicionarCafeManha(cafeManha);
+                return ResponseEntity.status(HttpStatus.CREATED).body("Café da Manhã associado ao Colaborador com sucesso");
+
             } catch (Exception e) {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                         .body("Erro ao associar Café da Manhã ao Colaborador.");
