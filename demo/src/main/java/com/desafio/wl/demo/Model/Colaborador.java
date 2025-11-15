d450008adb9bfce3104e7ba993b22cd717f98749
gmorais-dev <guilherme_morais14@hotmail.com>
2025-11-15 02:02:51 -0300

    refactor-@Table nas classes bases ( colaborador e cafemanha

--- demo/src/main/java/com/desafio/wl/demo/Model/Colaborador.java
+++ demo/src/main/java/com/desafio/wl/demo/Model/Colaborador.java
@@ -1,9 +1,6 @@
 package com.desafio.wl.demo.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "colaborador")
 public class Colaborador {
 
     @GeneratedValue(strategy = GenerationType.UUID)
